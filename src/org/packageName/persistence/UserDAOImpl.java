f734ae6123d630daf52bd31e703c498da789f78f
黄章延 <1503409765@qq.com>
2021-10-09 13:41:07 +0800

    稍微修改

--- src/org/packageName/persistence/UserDAOImpl.java
+++ src/org/packageName/persistence/UserDAOImpl.java
@@ -1,4 +1,38 @@
 package org.packageName.persistence;
 
-public class UserDAOImpl implements UserDAO{
+import org.packageName.domain.User;
+
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public User Login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUserList() {
+        return null;
+    }
+
+    @Override
+    public boolean updateUserInfo(User user) {
+        return false;
+    }
+
+    @Override
+    public int deleteUser() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        UserDAO tmp=new UserDAOImpl();
+        tmp.getUserById(123);
+        Math.abs(123);
+    }
 }
